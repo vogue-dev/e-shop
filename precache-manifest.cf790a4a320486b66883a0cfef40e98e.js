fc65faaeff80916c8defc859703b4c1f9d397eef
Vogue <oprya1111@yandex.ru>
2020-08-28 00:54:35 +0300

    Updates

---
+++ precache-manifest.cf790a4a320486b66883a0cfef40e98e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5064a15f597438edc3df3aa81d47f413",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "f68a41f53cb694a8e6ae",
+    "url": "/e-shop/static/css/main.1ee72e6f.chunk.css"
+  },
+  {
+    "revision": "90d3f8aaddd0796de037",
+    "url": "/e-shop/static/js/2.fa6f598e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-shop/static/js/2.fa6f598e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68a41f53cb694a8e6ae",
+    "url": "/e-shop/static/js/main.d0292791.chunk.js"
+  },
+  {
+    "revision": "68f078dd14ba28d9f667",
+    "url": "/e-shop/static/js/runtime-main.8f96e1d8.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/e-shop/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/e-shop/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/e-shop/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/e-shop/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/e-shop/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/e-shop/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/e-shop/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/e-shop/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/e-shop/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/e-shop/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/e-shop/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/e-shop/static/media/ProximaNova-Regular.b7c51278.eot"
+  }
+]);
