791bd23ef0dcf354d4cf26ad3f76459e822af217
Vogue <oprya1111@yandex.ru>
2020-09-02 17:29:12 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json');
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 5000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-
-const app = express();
-app.use(cors());
-//Bodyparser Middleware
-app.use(bodyParser.json());
-
-app.use('/api/items', require('./routes/api/items'));
-app.use('/api/users', require('./routes/api/users'));
-
-//Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	//set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
