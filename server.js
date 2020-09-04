18569093547a0a68ad09963f464adc90428dc500
Vogue <oprya1111@yandex.ru>
2020-09-04 22:30:26 +0300

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const middlewares = jsonServer.defaults({
+	static: './build',
+});
+
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+	console.log('Server is runing');
+});
