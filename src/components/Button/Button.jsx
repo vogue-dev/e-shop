5977d56f1908b53bbe85d50fb131283de4646737
Vogue <oprya1111@yandex.ru>
2020-09-08 13:35:22 +0300

    heroku

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,6 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 const Button = ({ onClick, className, outline, children }) => {
-	console.log('test git');
 	return (
 		<button
 			onClick={onClick}
